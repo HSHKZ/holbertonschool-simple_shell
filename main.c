b452fdd4c2b5ca8b0a0e615565ce7b20bd1a5061
lowbacc <skalski.matthis@gmail.com>
2024-08-19 10:04:44 +0200

    File containing the main function and the main code of the program

---
+++ main.c
@@ -0,0 +1,40 @@
+#include "shell.h"
+
+/**
+ * main - Entry point
+ * Return: 0 if success
+ */
+int main(void)
+{
+	char *command;
+	char **args;
+
+	while (1)
+	{
+		prompt();
+		command = read_input();
+
+		if (!command)
+		{
+			break;
+		}
+
+		command[strcspn(command, "\n")] = 0;
+
+		if (strcmp(command, "exit") == 0)
+		{
+			free(command);
+			break;
+		}
+
+		args = parse_command(command);
+
+		if (args[0] != NULL)
+			execute_command(args);
+
+		free(command);
+		free(args);
+	}
+
+	return (0);
+}
